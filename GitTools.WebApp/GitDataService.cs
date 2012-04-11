121dd554634b031242aef4819aede6903f57d6c3
Philip Stears <philip@driveworks.co.uk>
2012-04-11 21:09:20 +0100

    Changed reference to GitTools to a project reference instead of nuget

--- GitTools.WebApp/GitDataService.cs
+++ GitTools.WebApp/GitDataService.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Data.Services;
+﻿using System.Data.Services;
 using System.Data.Services.Common;
-using System.Collections.Generic;
-using System.Linq;
-using System.ServiceModel.Web;
-using GitTools.DataServices;
+using GitTools;
 
 public class GitDataService : DataService<GitDataSource>
 {
--- GitTools.WebSite/App_Code/GitDataService.cs
+++ GitTools.WebSite/App_Code/GitDataService.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Data.Services;
+﻿using System.Data.Services;
 using System.Data.Services.Common;
-using System.Collections.Generic;
-using System.Linq;
-using System.ServiceModel.Web;
 using GitTools;
 
 public class GitDataService : DataService<GitDataSource>
